02b9ec7832b5e9873f32dec60bf043179280edf6
Susan Minott <susan_minott@yahoo.com>
2023-03-24 16:49:55 -0400

    Cleanup

--- app.js
+++ app.js
@@ -13,18 +13,3 @@
         document.body.classList.toggle("light-mode");
     })
 })();
-
-// Option 2:
-// const controllBtn = document.querySelectorAll('.controll')
-// const controllBtns = document.querySelectorAll('.controlls')
-
-// function pageTransition(){
-//     for (let i = 0; 1 < controllBtn.length; i++){
-//         controllBtn[i].addEventListener('click', function(){
-//             let clickedbtn = document.querySelectorAll('.active-btn');
-//             clickedbtn[0].className = clickedbtn[0].className.replace('active-btn', '');
-//             this.className += 'active-btn';
-//         })
-//     }
-// }
-// pageTransition();
